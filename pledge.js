00e19a9af56db087ef1f6e1014c0823dca62829c
lesterhm <lesterechem@gmail.com>
2015-10-01 18:43:43 -0400

    Completed up to last part of Ch2

--- pledge.js
+++ pledge.js
@@ -3,6 +3,46 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+var $Promise = function() {
+  this.state = 'pending';
+  this.value = null;
+  this.handlerGroups = [];
+}
+
+$Promise.prototype.then = function(success, error){ 
+  if(typeof success !== 'function') success = null;
+  if(typeof error !== 'function') error = null;
+  this.handlerGroups.push({
+    successCb: success,
+    errorCb: error
+  });
+  if(this.state === 'resolved') return this.handlerGroups.shift().successCb(this.value);
+  if(this.state === 'rejected') return this.handlerGroups.shift().errorCb(this.value);
+}
+
+var Deferral = function() {
+  this.$promise = new $Promise();
+}
+
+Deferral.prototype.resolve = function(dataObj){
+  if(this.$promise.state === 'pending') {
+    this.$promise.value = dataObj;
+    this.$promise.state = 'resolved';
+    
+  } 
+
+}
+
+Deferral.prototype.reject = function(reason){
+  if(this.$promise.state === 'pending') {
+    this.$promise.value = reason;
+    this.$promise.state = 'rejected';
+  }
+}
+
+function defer() {
+  return new Deferral;
+}
 
 
 
